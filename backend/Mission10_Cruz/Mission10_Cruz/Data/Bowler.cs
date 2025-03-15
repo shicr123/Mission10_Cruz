4eae7b7ee46d692d60106199b9a08d26c25cad2e
shicr <shicr@byu.edu>
2025-03-14 23:14:32 -0600

    :)

--- backend/Mission10_Cruz/Mission10_Cruz/Data/Bowler.cs
+++ backend/Mission10_Cruz/Mission10_Cruz/Data/Bowler.cs
@@ -1,15 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Mission10_Cruz.Data;
 
-public class Bowler
-{
-    [Key]
-    public int BowlerID { get; set; }
-    public string BowlerLastName { get; set; }
-    public string teamName { get; set; }
-    public string Adress { get; set; }
-    public string City { get; set; }
-    public string State { get; set; }
-    public int Zip { get; set; }
-    public int Phone { get; set; }
+    public class Bowler
+    {
+        [Key]
+        public int BowlerID { get; set; }
+
+        [Required]
+        public string BowlerLastName { get; set; }
+
+        public string BowlerFirstName { get; set; }
+        public string BowlerMiddleInit { get; set; }
+        public string BowlerAddress { get; set; }
+        public string BowlerCity { get; set; }
+        public string BowlerState { get; set; }
     
-}
+        [Required]
+        public string BowlerZip { get; set; }  // Zip codes should be strings
+    
+        public string BowlerPhoneNumber { get; set; }  // Phone numbers should be strings
+    
+        public int TeamID { get; set; }  // Foreign Key reference to Team table
+    }
+
+
+    
